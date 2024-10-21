75465beac9e78192e08dc6a768e2af7e03677849
gruferok <gruferok46@mail.ru>
2024-10-21 22:32:11 +0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Настройки
+users_per_port=2
+total_ports=2
+
 # Снятие системных лимитов
 echo "Снятие системных лимитов..."
 ulimit -n 1048576
@@ -16,24 +20,19 @@ fi
 # Создание конфигурационного файла для Squid
 echo "Создание конфигурационного файла для Squid..."
 cat <<EOL > /etc/squid/squid.conf
-http_port 3128
-
 auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
-auth_param basic children 50 startup=5 idle=1
+auth_param basic children 5
 auth_param basic realm Squid proxy-caching web server
 auth_param basic credentialsttl 2 hours
 auth_param basic casesensitive off
-
 acl authenticated proxy_auth REQUIRED
 http_access allow authenticated
-http_access allow CONNECT
-http_access allow localhost
 
-positive_dns_ttl 5 minutes
-negative_dns_ttl 30 seconds
-connect_timeout 30 seconds
-
-cache_log /var/log/squid/cache.log
+# Уникальный IPv6 для каждого соединения
+acl connect method CONNECT
+external_acl_type ipv6_per_connection ipv6 %CONNECTION_ID
+acl unique_ipv6 external ipv6_per_connection
+tcp_outgoing_address 2a10:9680:1::%o unique_ipv6
 EOL
 
 # Генерация случайных логинов и паролей, и добавление их в файл паролей
@@ -41,35 +40,39 @@ echo "Генерация случайных логинов и паролей..."
 touch /etc/squid/passwd
 touch /etc/squid/proxies.txt
 
-for i in {1..1000}
-do
-    username="user$i"
-    password=$(openssl rand -base64 12)
+total_users=$((users_per_port * total_ports))
+for port in $(seq 3128 $((3127 + total_ports))); do
+    echo "http_port $port" >> /etc/squid/squid.conf
     
-    if [ $i -eq 1 ]; then
-        htpasswd -c -b /etc/squid/passwd $username $password
-    else
-        htpasswd -b /etc/squid/passwd $username $password
-    fi
-    
-    if [ $? -ne 0 ]; then
-        echo "Ошибка при создании пользователя $username!"
-        exit 1
-    fi
-    
-    ipv6="2a10:9680:1::$(printf '%x' $i)"
-    echo "45.87.246.238:3128:$username:$password" >> /etc/squid/proxies.txt
-    echo "acl user_$i proxy_auth $username" >> /etc/squid/squid.conf
-    echo "tcp_outgoing_address $ipv6 user_$i" >> /etc/squid/squid.conf
+    for i in $(seq 1 $users_per_port); do
+        user_id=$((($port - 3128) * users_per_port + i))
+        username="user$user_id"
+        password=$(openssl rand -base64 12)
+        
+        if [ $user_id -eq 1 ]; then
+            htpasswd -c -b /etc/squid/passwd $username $password
+        else
+            htpasswd -b /etc/squid/passwd $username $password
+        fi
+        
+        if [ $? -ne 0 ]; then
+            echo "Ошибка при создании пользователя $username!"
+            exit 1
+        fi
+        
+        echo "45.87.246.238:$port:$username:$password" >> /etc/squid/proxies.txt
+    done
 done
 
-# Перезапуск Squid для применения изменений
-echo "Перезапуск Squid..."
-systemctl restart squid
-if [ $? -ne 0 ]; then
-    echo "Ошибка при перезапуске Squid!"
-    exit 1
-fi
+# Создание скрипта для генерации уникальных IPv6
+cat <<'EOL' > /usr/lib/squid/ipv6_per_connection
+#!/bin/bash
+while read line; do
+    echo $(printf '2a10:9680:1::%x' $((RANDOM % 65536)))
+done
+EOL
+
+chmod +x /usr/lib/squid/ipv6_per_connection
 
 # Скрипт для ротации IPv6 адресов каждые 5 минут
 echo "Создание крон-задачи для ротации IPv6 адресов..."
@@ -80,11 +83,6 @@ EOL
 # Скрипт для ротации IPv6 адресов
 cat <<'EOL' > /usr/bin/rotate_squid_ipv6.sh
 #!/bin/bash
-for i in {1..1000}
-do
-    ipv6="2a10:9680:1::$(printf '%x' $((i + RANDOM % 1000)))"
-    sed -i "s/tcp_outgoing_address .* user_$i/tcp_outgoing_address $ipv6 user_$i/" /etc/squid/squid.conf
-done
 systemctl reload squid
 if [ $? -ne 0 ]; then
     echo "Ошибка при перезагрузке Squid!"
@@ -95,34 +93,12 @@ EOL
 # Настройка прав на выполнение скрипта
 chmod +x /usr/bin/rotate_squid_ipv6.sh
 
+# Перезапуск Squid для применения изменений
+echo "Перезапуск Squid..."
+systemctl restart squid
+if [ $? -ne 0 ]; then
+    echo "Ошибка при перезапуске Squid!"
+    exit 1
+fi
+
 echo "Установка и настройка завершены! Прокси сохранены в /etc/squid/proxies.txt"
-
-# Скрипт для мониторинга сервера и предупреждений при перегрузке
-cat <<'EOL' > /usr/bin/monitor_server.sh
-#!/bin/bash
-CPU_THRESHOLD=80
-MEM_THRESHOLD=80
-while true; do
-    CPU_USAGE=$(top -b -n1 | grep "Cpu(s)" | awk '{print $2 + $4}')
-    MEM_USAGE=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
-    
-    if (( $(echo "$CPU_USAGE > $CPU_THRESHOLD" | bc -l) )); then
-        echo "Предупреждение: Загрузка CPU превышает $CPU_THRESHOLD%. Текущая загрузка: $CPU_USAGE%."
-    fi
-    
-    if (( $(echo "$MEM_USAGE > $MEM_THRESHOLD" | bc -l) )); then
-        echo "Предупреждение: Загрузка памяти превышает $MEM_THRESHOLD%. Текущая загрузка: $MEM_USAGE%."
-    fi
-    
-    sleep 60
-done
-EOL
-
-# Настройка прав на выполнение скрипта
-chmod +x /usr/bin/monitor_server.sh
-
-# Создание крон-задачи для мониторинга сервера
-echo "Создание крон-задачи для мониторинга сервера..."
-cat <<EOL > /etc/cron.d/monitor_server
-* * * * * root /usr/bin/monitor_server.sh
-EOL
