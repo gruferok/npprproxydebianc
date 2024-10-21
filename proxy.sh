d327f8baa3353d4acc7bfc5b9651e0f22aabe1fb
gruferok <gruferok46@mail.ru>
2024-10-21 21:56:48 +0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -5,6 +5,10 @@ echo "Снятие системных лимитов..."
 ulimit -n 1048576
 ulimit -u 1048576
 
+# Запрос количества пользователей
+echo "Сколько пользователей вы хотите сгенерировать?"
+read user_count
+
 # Установка Squid
 echo "Установка Squid..."
 apt-get update && apt-get install -y squid apache2-utils
@@ -17,26 +21,46 @@ fi
 echo "Создание конфигурационного файла для Squid..."
 cat <<EOL > /etc/squid/squid.conf
 http_port 3128
+
 auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
-auth_param basic children 5
+auth_param basic children 50 startup=5 idle=1
 auth_param basic realm Squid proxy-caching web server
 auth_param basic credentialsttl 2 hours
 auth_param basic casesensitive off
 
 acl authenticated proxy_auth REQUIRED
 http_access allow authenticated
+http_access allow CONNECT
+http_access allow localhost
 
-# Параметры для логирования
-access_log /var/log/squid/access.log squid
+positive_dns_ttl 5 minutes
+negative_dns_ttl 30 seconds
+connect_timeout 30 seconds
+
 cache_log /var/log/squid/cache.log
+
+# Внешний ACL для генерации уникальных IPv6 адресов
+external_acl_type ipv6_generator %LOGIN /usr/local/bin/generate_ipv6.sh
+acl dynamic_ipv6 external ipv6_generator
+tcp_outgoing_address 2a10:9680:1::%>a dynamic_ipv6
 EOL
+
+# Создание скрипта для генерации уникальных IPv6 адресов
+cat <<'EOL' > /usr/local/bin/generate_ipv6.sh
+#!/bin/bash
+while read login; do
+    printf "%x\n" $((RANDOM << 15 | RANDOM))
+done
+EOL
+
+chmod +x /usr/local/bin/generate_ipv6.sh
 
 # Генерация случайных логинов и паролей, и добавление их в файл паролей
 echo "Генерация случайных логинов и паролей..."
 touch /etc/squid/passwd
 touch /etc/squid/proxies.txt
 
-for i in {1..50}
+for i in $(seq 1 $user_count)
 do
     username="user$i"
     password=$(openssl rand -base64 12)
@@ -52,10 +76,7 @@ do
         exit 1
     fi
     
-    ipv6="2a10:9680:1::$(printf '%x' $i)"
     echo "45.87.246.238:3128:$username:$password" >> /etc/squid/proxies.txt
-    echo "acl user_$i proxy_auth $username" >> /etc/squid/squid.conf
-    echo "tcp_outgoing_address $ipv6 user_$i" >> /etc/squid/squid.conf
 done
 
 # Перезапуск Squid для применения изменений
@@ -65,29 +86,5 @@ if [ $? -ne 0 ]; then
     echo "Ошибка при перезапуске Squid!"
     exit 1
 fi
-
-# Скрипт для ротации IPv6 адресов каждые 5 минут
-echo "Создание крон-задачи для ротации IPv6 адресов..."
-cat <<EOL > /etc/cron.d/rotate_squid_ipv6
-*/5 * * * * root /usr/bin/rotate_squid_ipv6.sh
-EOL
-
-# Скрипт для ротации IPv6 адресов
-cat <<'EOL' > /usr/bin/rotate_squid_ipv6.sh
-#!/bin/bash
-for i in {1..50}
-do
-    ipv6="2a10:9680:1::$(printf '%x' $((i + RANDOM % 50)))"
-    sed -i "s/tcp_outgoing_address .*/tcp_outgoing_address $ipv6 user_$i/" /etc/squid/squid.conf
-done
-systemctl reload squid
-if [ $? -ne 0 ]; then
-    echo "Ошибка при перезагрузке Squid!"
-    exit 1
-fi
-EOL
-
-# Настройка прав на выполнение скрипта
-chmod +x /usr/bin/rotate_squid_ipv6.sh
 
 echo "Установка и настройка завершены! Прокси сохранены в /etc/squid/proxies.txt"
