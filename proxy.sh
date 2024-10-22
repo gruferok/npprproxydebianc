bd4c6907c826805d91ff7aaf29f70b8b15a477b2
gruferok <gruferok46@mail.ru>
2024-10-22 15:25:10 +0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,243 +1,84 @@
 #!/bin/bash
 
-# Функция для отображения финального сообщения
-show_final_message() {
-    local local_path=$1
-    echo "Файл с прокси сохранен по адресу: ${local_path}"
-}
-
-# Функция для интерактивного ввода параметров
-get_user_input() {
-    echo "Пожалуйста, введите следующие параметры для установки прокси:"
-    
-    # Подсеть
-    echo "Подсеть (например, 48):"
-    read subnet
-    if [[ -z "$subnet" ]]; then subnet=64; fi
-    
-    # Логин и пароль
-    echo "Логин и пароль:"
-    echo "1) Указать"
-    echo "2) Без логина и пароля"
-    echo "3) Рандомные"
-    read auth_choice
-    case $auth_choice in
-        1)
-            echo "Введите логин:"
-            read user
-            echo "Введите пароль:"
-            read password
-            use_random_auth=false
-            ;;
-        2)
-            user=""
-            password=""
-            use_random_auth=false
-            ;;
-        3)
-            user=""
-            password=""
-            use_random_auth=true
-            ;;
-        *)
-            echo "Неверный выбор, будут использованы рандомные логин и пароль."
-            use_random_auth=true
-            ;;
-    esac
-    
-    # Количество прокси
-    echo "Количество прокси:"
-    read proxy_count
-    if [[ -z "$proxy_count" ]]; then proxy_count=100; fi
-}
-
-# Функция для проверки параметров запуска
-check_startup_parameters() {
-    if ! [[ $proxy_count =~ ^[0-9]+$ ]]; then
-        echo "Ошибка: Количество прокси должно быть положительным целым числом" >&2
-        exit 1
-    fi
-
-    if [ $(expr $subnet % 4) != 0 ]; then
-        echo "Ошибка: Значение подсети должно быть кратно 4" >&2
-        exit 1
-    fi
-}
-
-# Функция для проверки IPv6
-check_ipv6() {
-    if ! test -f /proc/net/if_inet6; then
-        echo "Ошибка: IPv6 интерфейс не включен. Включите IPv6 в вашей системе." >&2
-        exit 1
-    fi
-
-    if ! [[ $(ip -6 addr show scope global) ]]; then
-        echo "Ошибка: Глобальный IPv6 адрес не назначен серверу." >&2
-        exit 1
-    fi
-}
-
-# Функция для настройки IPv6
-configure_ipv6() {
-    echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-    echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
-    sysctl -p
-}
-
-# Функция для установки необходимых пакетов
-install_required_packages() {
-    apt update
-    apt install -y make g++ wget curl
-}
-
-# Функция для установки 3proxy
-install_3proxy() {
-    mkdir -p ~/proxyserver && cd ~/proxyserver
-    wget https://github.com/3proxy/3proxy/archive/0.9.4.tar.gz
-    tar -xvf 0.9.4.tar.gz
-    cd 3proxy-0.9.4
-    make -f Makefile.Linux
-    mkdir -p ~/proxyserver/3proxy
-    mv bin/3proxy ~/proxyserver/3proxy/
-    cd ~/proxyserver
-    rm -rf 3proxy-0.9.4 0.9.4.tar.gz
-}
-
-# Функция для получения IPv4 адреса для обратного подключения
-get_backconnect_ipv4() {
-    ip addr show | awk '$1 == "inet" && $3 == "brd" {gsub(/\/.*$/, "", $2); print $2; exit}'
-}
-
-# Функция для генерации случайных пользователей, если необходимо
-generate_random_users_if_needed() {
-    if [ "$use_random_auth" = true ]; then
-        for i in $(seq 1 $proxy_count); do
-            echo "user$i:$(openssl rand -base64 12)" >> ~/proxyserver/users.txt
-        done
-    fi
-}
-
-# Функция для создания скрипта запуска
-create_startup_script() {
-    cat > ~/proxyserver/start.sh <<EOL
-#!/bin/bash
-ulimit -n 600000
-ulimit -u 600000
-~/proxyserver/3proxy/3proxy ~/proxyserver/3proxy.cfg
-EOL
-    chmod +x ~/proxyserver/start.sh
-}
-
-# Функция для добавления в cron
-add_to_cron() {
-    (crontab -l 2>/dev/null; echo "@reboot ~/proxyserver/start.sh") | crontab -
-}
-
-# Функция для запуска прокси-сервера
-run_proxy_server() {
-    ~/proxyserver/start.sh
-}
-
-# Функция для записи прокси в файл
+# Function to write proxies to file
 write_backconnect_proxies_to_file() {
-    local proxy_file="$1"
-    local backconnect_ip=$(get_backconnect_ipv4)
-    local start_port=30000
-    
-    for i in $(seq 1 $proxy_count); do
-        local port=$((start_port + i - 1))
-        if [ "$use_random_auth" = true ]; then
-            local credentials=$(sed -n "${i}p" ~/proxyserver/users.txt)
-            echo "$backconnect_ip:$port:$credentials" >> "$proxy_file"
-        else
-            if [ -n "$user" ] && [ -n "$password" ]; then
-                echo "$backconnect_ip:$port:$user:$password" >> "$proxy_file"
-            else
-                echo "$backconnect_ip:$port" >> "$proxy_file"
-            fi
-        fi
+  delete_file_if_exists $proxy_dir/proxy.txt
+
+  for port in $(eval echo "{$start_port..$last_port}"); do
+    echo "$backconnect_ipv4:$port" >> $proxy_dir/proxy.txt
+  done
+}
+
+# Main script
+proxy_dir="$HOME/proxyserver"
+mkdir -p $proxy_dir
+
+# Set default values
+subnet=48
+proxies_type="http"
+start_port=30000
+proxy_count=10
+mode_flag="-6"
+
+# Install required packages
+apt update
+apt install -y make g++ wget curl
+
+# Download and build 3proxy
+cd $proxy_dir
+wget https://github.com/3proxy/3proxy/archive/refs/tags/0.9.4.tar.gz
+tar -xf 0.9.4.tar.gz
+rm 0.9.4.tar.gz
+mv 3proxy-0.9.4 3proxy
+cd 3proxy
+make -f Makefile.Linux
+
+# Configure IPv6
+interface_name=$(ip -br l | awk '$1 !~ "lo|vir|wl|@NONE" { print $1 }' | awk 'NR==1')
+echo "net.ipv6.conf.$interface_name.proxy_ndp=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
+echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
+echo "net.ipv6.ip_nonlocal_bind=1" >> /etc/sysctl.conf
+sysctl -p
+
+# Generate random IPv6 addresses
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+rnd_subnet_ip() {
+    echo -n 2001:0:ae1e:$(printf "%x\n" $((RANDOM % 65536))):$(printf "%x\n" $((RANDOM % 65536))):$(printf "%x\n" $((RANDOM % 65536))):
+    for i in {1..4}; do
+        echo -n ${array[$RANDOM % 16]}
     done
 }
 
-# Функция для получения IPv6 адреса
-get_ipv6_address() {
-    ip -6 addr show scope global | grep -oP '(?<=inet6\s)\S+'
-}
+for i in $(seq 1 $proxy_count); do
+    rnd_subnet_ip >> $proxy_dir/ipv6.list
+done
 
-# Функция для генерации IPv6 адресов
-generate_ipv6_addresses() {
-    local ipv6_addr=$(get_ipv6_address | cut -d'/' -f1)
-    local subnet_mask=$(get_ipv6_address | cut -d'/' -f2)
-    local ipv6_prefix=$(echo $ipv6_addr | cut -d':' -f1-4)
-
-    for i in $(seq 1 $proxy_count); do
-        echo "${ipv6_prefix}:$(printf '%04x:%04x' $((RANDOM%65535)) $((RANDOM%65535)))/${subnet_mask}"
-    done
-}
-
-# Функция для создания конфигурационного файла 3proxy
-create_3proxy_config() {
-    local ipv6_addresses=($(generate_ipv6_addresses))
-    
-    cat > ~/proxyserver/3proxy.cfg <<EOL
+# Create 3proxy config
+cat > $proxy_dir/3proxy.cfg <<EOF
 daemon
-maxconn 2000
 nserver 1.1.1.1
-nserver 8.8.8.8
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 setgid 65535
 setuid 65535
 stacksize 6291456 
 flush
-auth $([[ "$use_random_auth" = true ]] && echo "strong" || echo "none")
-users $([ -f ~/proxyserver/users.txt ] && cat ~/proxyserver/users.txt | tr '\n' ' ' || echo "$user:CL:$password")
-allow * * * *
-$(for i in $(seq 1 $proxy_count); do
-    echo "proxy -6 -n -a -p$((30000 + i - 1)) -i${ipv6_addresses[$((i-1))]%/*} -e"
-done)
-EOL
-}
+auth none
 
-# Функция для настройки дополнительных IPv6 адресов
-setup_additional_ipv6() {
-    local ipv6_addresses=($(generate_ipv6_addresses))
-    local interface=$(ip -6 route | grep default | awk '{print $5}')
+$(awk -v port="$start_port" '{print "proxy -6 -n -a -p"port" -i127.0.0.1 -e"$0; port++}' $proxy_dir/ipv6.list)
+EOF
 
-    for addr in "${ipv6_addresses[@]}"; do
-        ip -6 addr add $addr dev $interface
-    done
-}
+# Start 3proxy
+ulimit -n 600000
+ulimit -u 600000
+$proxy_dir/3proxy/bin/3proxy $proxy_dir/3proxy.cfg &
 
-# Основной код скрипта
-get_user_input
-check_startup_parameters
-check_ipv6
-configure_ipv6
-install_required_packages
-install_3proxy
-generate_random_users_if_needed
-create_startup_script
-add_to_cron
+# Write proxies to file
+backconnect_ipv4=$(curl -s https://ipinfo.io/ip)
+last_port=$((start_port + proxy_count - 1))
+write_backconnect_proxies_to_file
 
-# Настройка дополнительных IPv6 адресов
-setup_additional_ipv6
-
-# Создание конфигурационного файла для 3proxy
-create_3proxy_config
-
-# Запуск прокси-сервера
-run_proxy_server
-
-# Путь к файлу с прокси
-proxy_file="$HOME/proxy_list.txt"
-
-# Сохраняем прокси в текстовый файл
-write_backconnect_proxies_to_file "$proxy_file"
-
-# Вызов функции для отображения финального сообщения
-show_final_message "$proxy_file"
-
-exit 0
+# Display final message
+echo "Файл с прокси создан по адресу - $proxy_dir/proxy.txt"
