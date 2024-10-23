518496103beb913c798f2c1636a2bbb77879e2a7
gruferok <gruferok46@mail.ru>
2024-10-23 09:00:04 +0300

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -804,31 +804,52 @@ if [ $uninstall = true ]; then
 fi;
 
 # Выполняем первоначальные настройки
-echo "* hard nofile 999999" >> /etc/security/limits.conf
-echo "* soft nofile 999999" >> /etc/security/limits.conf
-echo "net.ipv4.route.min_adv_mss = 1460" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_timestamps=0" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_window_scaling=0" >> /etc/sysctl.conf
-echo "net.ipv4.icmp_echo_ignore_all = 1" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_max_syn_backlog = 4096" >> /etc/sysctl.conf
-echo "net.ipv4.conf.all.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv4.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.proxy_ndp=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.default.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv6.conf.all.forwarding=1" >> /etc/sysctl.conf
-echo "net.ipv6.ip_nonlocal_bind = 1" >> /etc/sysctl.conf
+
+echo "* hard nofile 1048576" >> /etc/security/limits.conf
+echo "* soft nofile 1048576" >> /etc/security/limits.conf
+echo "* hard nproc 1048576" >> /etc/security/limits.conf
+echo "* soft nproc 1048576" >> /etc/security/limits.conf
+
+cat << EOF >> /etc/sysctl.conf
+net.ipv4.route.min_adv_mss = 1460
+net.ipv4.tcp_timestamps = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.icmp_echo_ignore_all = 0
+net.ipv4.tcp_max_syn_backlog = 1000000
+net.ipv4.conf.all.forwarding = 1
+net.ipv4.ip_nonlocal_bind = 1
+net.ipv6.conf.all.proxy_ndp = 1
+net.ipv6.conf.default.forwarding = 1
+net.ipv6.conf.all.forwarding = 1
+net.ipv6.ip_nonlocal_bind = 1
+
+# Оптимизация TCP/IP для высокой производительности
+net.ipv4.ip_default_ttl = 64
+net.ipv4.tcp_syn_retries = 3
+net.ipv4.tcp_fin_timeout = 15
+net.ipv4.tcp_keepalive_time = 300
+net.ipv4.tcp_rmem = 4096 87380 16777216
+net.ipv4.tcp_wmem = 4096 65536 16777216
+net.core.rmem_max = 16777216
+net.core.wmem_max = 16777216
+net.ipv4.tcp_mem = 16777216 16777216 16777216
+net.core.netdev_max_backlog = 1000000
+net.ipv4.tcp_max_tw_buckets = 1440000
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_low_latency = 1
+net.ipv4.ip_local_port_range = 1024 65535
+EOF
+
 sysctl -p
+
 systemctl stop firewalld
 systemctl disable firewalld
 
-# Настройки TCP/IP для имитации Windows
-echo "net.ipv4.ip_default_ttl=128" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_syn_retries=2" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_fin_timeout=30" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_keepalive_time=7200" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_rmem=4096 87380 6291456" >> /etc/sysctl.conf
-echo "net.ipv4.tcp_wmem=4096 16384 6291456" >> /etc/sysctl.conf
-sysctl -p
 
 delete_file_if_exists $script_log_file;
 check_startup_parameters;
