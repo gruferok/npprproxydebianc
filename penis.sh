99778187670eb244bf40471b420975a81e910e30
gruferok <gruferok46@mail.ru>
2024-10-23 17:03:36 +0300

    Update penis.sh

--- penis.sh
+++ penis.sh
@@ -1,42 +1,85 @@
-su -c '
+#!/bin/bash
 
-# Удаление директорий и файлов
-rm -rf /root/proxyserver /var/tmp/ipv6-proxy-server-install.log /var/tmp/ipv6-proxy-server-logs.log
+# Установка Squid
+echo "Установка Squid..."
+apt-get update && apt-get install -y squid apache2-utils
+if [ $? -ne 0 ]; then
+    echo "Ошибка при установке Squid!"
+    exit 1
+fi
 
+# Создание конфигурационного файла для Squid
+echo "Создание конфигурационного файла для Squid..."
+cat <<EOL > /etc/squid/squid.conf
+http_port 3128
+auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
+auth_param basic children 5
+auth_param basic realm Squid proxy-caching web server
+auth_param basic credentialsttl 2 hours
+auth_param basic casesensitive off
 
-# Удаление IPv6 адресов
-for ipv6 in $(ip -6 addr show scope global | grep inet6 | awk "{print \$2}"); do
-    ip -6 addr del $ipv6 dev $(ip -6 route | grep default | awk "{print \$5}")
+acl authenticated proxy_auth REQUIRED
+http_access allow authenticated
+EOL
+
+# Генерация случайных логинов и паролей, и добавление их в файл паролей
+echo "Генерация случайных логинов и паролей..."
+touch /etc/squid/passwd
+touch /etc/squid/proxies.txt
+
+for i in {1..10}  # Изменено на 10
+do
+    username="user$i"
+    password=$(openssl rand -base64 12)
+
+    if [ $i -eq 1 ]; then
+        htpasswd -c -b /etc/squid/passwd $username $password
+    else
+        htpasswd -b /etc/squid/passwd $username $password
+    fi
+
+    if [ $? -ne 0 ]; then
+        echo "Ошибка при создании пользователя $username!"
+        exit 1
+    fi
+
+    ipv6="2a10:9680:1::$(printf '%x' $i)"
+    echo "http_port 45.87.246.238:$((30296 + $i)) name=proxy$i" >> /etc/squid/squid.conf
+    echo "45.87.246.238:$((30296 + $i)):$username:$password" >> /etc/squid/proxies.txt
+    echo "acl ipv6_$i myportname proxy$i" >> /etc/squid/squid.conf
+    echo "tcp_outgoing_address $ipv6 ipv6_$i" >> /etc/squid/squid.conf
 done
 
-# Очистка изменений в /etc/security/limits.conf
-sed -i "/\* hard nofile 999999/d" /etc/security/limits.conf
-sed -i "/\* soft nofile 999999/d" /etc/security/limits.conf
+# Перезапуск Squid для применения изменений
+echo "Перезапуск Squid..."
+systemctl restart squid
+if [ $? -ne 0 ]; then
+    echo "Ошибка при перезапуске Squid!"
+    exit 1
+fi
 
-# Очистка изменений в /etc/sysctl.conf
-sed -i "/net.ipv4.route.min_adv_mss = 1460/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_timestamps=0/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_window_scaling=0/d" /etc/sysctl.conf
-sed -i "/net.ipv4.icmp_echo_ignore_all = 1/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_max_syn_backlog = 4096/d" /etc/sysctl.conf
-sed -i "/net.ipv4.conf.all.forwarding=1/d" /etc/sysctl.conf
-sed -i "/net.ipv4.ip_nonlocal_bind = 1/d" /etc/sysctl.conf
-sed -i "/net.ipv6.conf.all.proxy_ndp=1/d" /etc/sysctl.conf
-sed -i "/net.ipv6.conf.default.forwarding=1/d" /etc/sysctl.conf
-sed -i "/net.ipv6.conf.all.forwarding=1/d" /etc/sysctl.conf
-sed -i "/net.ipv6.ip_nonlocal_bind = 1/d" /etc/sysctl.conf
-sed -i "/net.ipv4.ip_default_ttl=128/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_syn_retries=2/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_fin_timeout=30/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_keepalive_time=7200/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_rmem=4096 87380 6291456/d" /etc/sysctl.conf
-sed -i "/net.ipv4.tcp_wmem=4096 16384 6291456/d" /etc/sysctl.conf
+# Скрипт для ротации IPv6 адресов каждые 5 минут
+echo "Создание крон-задачи для ротации IPv6 адресов..."
+cat <<EOL > /etc/cron.d/rotate_squid_ipv6
+*/5 * * * * root /usr/bin/rotate_squid_ipv6.sh
+EOL
 
-# Применение изменений sysctl
-sysctl -p
+# Скрипт для ротации IPv6 адресов
+cat <<EOL > /usr/bin/rotate_squid_ipv6.sh
+#!/bin/bash
+for i in {1..10}  # Изменено на 10
+do
+    ipv6="2a10:9680:1::\$(printf '%x' \$((i + RANDOM % 1000)))"
+    sed -i "s/tcp_outgoing_address .*/tcp_outgoing_address \$ipv6 ipv6_$i/" /etc/squid/squid.conf
+done
+systemctl reload squid
+if [ \$? -ne 0 ]; then
+    echo "Ошибка при перезагрузке Squid!"
+    exit 1
+fi
+EOL
 
-# Удаление архива и информационных файлов
-rm -f /root/proxy.zip /root/upload_info.txt
+# Настройка прав на выполнение скрипта
+chmod +x /usr/bin/rotate_squid_ipv6.sh
 
-echo "Очистка завершена."
-'
+echo "Установка и настройка завершены! Прокси сохранены в /etc/squid/proxies.txt"
