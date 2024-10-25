d9eb36c33a28b61ea82e56d3cb095a7c401271b0
gruferok <gruferok46@mail.ru>
2024-10-25 16:33:37 +0300

    Update penis.sh

--- penis.sh
+++ penis.sh
@@ -5,6 +5,14 @@ log_message() {
     echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
+# Функция генерации случайного IPv6
+generate_random_ipv6() {
+    printf "2a10:9680:1:%04x:%04x:%04x:%04x:%04x" \
+        $((RANDOM % 65535)) $((RANDOM % 65535)) \
+        $((RANDOM % 65535)) $((RANDOM % 65535)) \
+        $((RANDOM % 65535))
+}
+
 # Функция проверки команды
 check_command() {
     if [ $? -ne 0 ]; then
@@ -27,76 +35,132 @@ apt-get update
 apt-get install -y squid apache2-utils iputils-ping net-tools curl
 check_command "Установка пакетов"
 
+# Создание необходимых директорий
+mkdir -p /usr/local/squid/var/run/
+mkdir -p /usr/local/squid/libexec/
+ln -sf /usr/lib/squid/basic_ncsa_auth /usr/local/squid/libexec/basic_ncsa_auth
+
 # Создание базового конфигурационного файла
 log_message "Создание конфигурационного файла для Squid..."
-cat <<EOL > /etc/squid/squid.conf
-# Базовые настройки
-pid_filename /var/run/squid.pid
+cat <<'EOL' > /etc/squid/squid.conf
+max_filedesc 500000
 
-# Отключение логов
-access_log none
-cache_store_log none
-cache deny all
+pid_filename /usr/local/squid/var/run/squidv6.pid
 
-# DNS серверы
-dns_nameservers 2001:4860:4860::8888 2001:4860:4860::8844
+access_log          none
 
-# ACL для IPv6
+cache_store_log     none
+
+# Hide client ip #
+forwarded_for delete
+
+# Turn off via header #
+via off
+
+# Deny request for original source of a request
+follow_x_forwarded_for allow localhost
+follow_x_forwarded_for deny all
+
+# See below
+request_header_access X-Forwarded-For deny all
+request_header_access Authorization allow all
+request_header_access Proxy-Authorization allow all
+request_header_access Cache-Control allow all
+request_header_access Content-Length allow all
+request_header_access Content-Type allow all
+request_header_access Date allow all
+request_header_access Host allow all
+request_header_access If-Modified-Since allow all
+request_header_access Pragma allow all
+request_header_access Accept allow all
+request_header_access Accept-Charset allow all
+request_header_access Accept-Encoding allow all
+request_header_access Accept-Language allow all
+request_header_access Connection allow all
+request_header_access All deny all
+
+cache           deny    all
+
 acl to_ipv6 dst ipv6
-http_access allow to_ipv6
 
-# Базовые порты
+http_access deny all !to_ipv6
+
+acl allow_net src 1.1.1.1
+
+# Common settings
 acl SSL_ports port 443
-acl Safe_ports port 80
-acl Safe_ports port 443
+acl Safe_ports port 80      # http
+acl Safe_ports port 21      # ftp
+acl Safe_ports port 443     # https
+acl Safe_ports port 70      # gopher
+acl Safe_ports port 210     # wais
+acl Safe_ports port 1025-65535  # unregistered ports
+acl Safe_ports port 280     # http-mgmt
+acl Safe_ports port 488     # gss-http
+acl Safe_ports port 591     # filemaker
+acl Safe_ports port 777     # multiling http
 acl CONNECT method CONNECT
 
-# Правила доступа
 http_access deny !Safe_ports
 http_access deny CONNECT !SSL_ports
+http_access allow localhost manager
+http_access deny manager
 
-# Аутентификация
-auth_param basic program /usr/lib/squid/basic_ncsa_auth /etc/squid/passwd
-auth_param basic realm Proxy
-acl authenticated proxy_auth REQUIRED
-http_access allow authenticated
+auth_param basic program /usr/local/squid/libexec/basic_ncsa_auth /etc/squid/squidv6.auth
+auth_param basic children 5
+auth_param basic realm Web-Proxy
+auth_param basic credentialsttl 1 minute
+auth_param basic casesensitive off
+
+acl db-auth proxy_auth REQUIRED
+http_access allow db-auth
+http_access allow localhost
 http_access deny all
 
-# Оптимизация
-visible_hostname V6proxies-Net
-EOL
+coredump_dir /var/spool/squid3
 
-check_command "Создание базовой конфигурации Squid"
+unique_hostname V6proxies-Net
+visible_hostname V6proxies-Net
+
+refresh_pattern ^ftp:       1440    20% 10080
+refresh_pattern ^gopher:    1440    0%  1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0%  0
+refresh_pattern .       0   20% 4320
+
+EOL
 
 # Генерация прокси
 log_message "Генерация прокси..."
-touch /etc/squid/passwd
+touch /etc/squid/squidv6.auth
 touch /etc/squid/proxies.txt
 
-for i in {1..10}
+for i in {0..10}
 do
     username="user$i"
     password=$(openssl rand -base64 12)
+    port=$((10000 + $i))
+    ipv6_addr=$(generate_random_ipv6)
     
     # Создание пользователей
-    if [ $i -eq 1 ]; then
-        htpasswd -c -b /etc/squid/passwd $username $password
+    if [ $i -eq 0 ]; then
+        htpasswd -c -b /etc/squid/squidv6.auth $username $password
     else
-        htpasswd -b /etc/squid/passwd $username $password
+        htpasswd -b /etc/squid/squidv6.auth $username $password
     fi
     check_command "Создание пользователя $username"
 
-    # Настройка порта и ACL
-    port=$((32000 + $i))
+    # Добавление конфигурации прокси
     cat <<EOL >> /etc/squid/squid.conf
-http_port [2a10:9680:1::$i]:$port
-acl p${port} localport $port
-tcp_outgoing_address 2a10:9680:1::$i p${port}
+
+http_access allow allow_net
+http_port       $port
+acl     p$port  localport       $port
+tcp_outgoing_address    $ipv6_addr p$port
 EOL
     check_command "Настройка прокси $i"
     
     # Сохранение данных прокси
-    echo "[2a10:9680:1::$i]:$port:$username:$password" >> /etc/squid/proxies.txt
+    echo "$ipv6_addr:$port:$username:$password" >> /etc/squid/proxies.txt
 done
 
 # Настройка IPv6
@@ -107,12 +171,13 @@ check_command "Очистка IPv6 настроек"
 
 # Настройка интерфейса
 ip link set dev ens3 up
-ip -6 addr add 2a10:9680:1::1/48 dev ens3
-ip -6 addr add fe80::1/64 dev ens3 scope link
 
-for i in {2..10}; do
-    ip -6 addr add 2a10:9680:1::$i/48 dev ens3
-done
+# Добавляем IPv6 адреса из proxies.txt
+while IFS=: read -r host port user pass; do
+    ip -6 addr add $host/48 dev ens3
+done < /etc/squid/proxies.txt
+
+ip -6 addr add fe80::1/64 dev ens3 scope link
 check_command "Настройка адресов интерфейса"
 
 # Маршрутизация
@@ -152,14 +217,13 @@ check_command "Запуск Squid"
 # Проверка прокси
 log_message "Начинаем проверку прокси..."
 
-i=1
 while IFS=: read -r host port user pass; do
     log_message "Тестирование прокси $host:$port"
     
-    if nc -z -w5 "${host//\[/}" "${port//\]/}"; then
+    if nc -z -w5 "$host" "$port"; then
         log_message "Порт $port открыт"
         
-        response=$(curl -6 --proxy-insecure --proxy "$host:$port" --proxy-user "$user:$pass" -s "https://api6.ipify.org" --connect-timeout 10 --interface 2a10:9680:1::$i)
+        response=$(curl -6 --proxy-insecure --proxy "[$host]:$port" --proxy-user "$user:$pass" -s "https://api6.ipify.org" --connect-timeout 10)
         
         if [[ $response == *"2a10"* ]]; then
             log_message "Прокси $host:$port РАБОТАЕТ (IPv6: $response)"
@@ -173,7 +237,6 @@ while IFS=: read -r host port user pass; do
         echo "$host:$port - ОШИБКА (Порт закрыт)" >> /root/failed_proxies.txt
     fi
     
-    i=$((i+1))
     sleep 1
 done < /etc/squid/proxies.txt
 
